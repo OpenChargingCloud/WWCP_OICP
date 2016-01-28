18a6efd70c928adab357cf36309038516395f183
Achim Friedland <github@ahzf.de>
2016-01-28 15:55:17 +0100

    Minor updates...

--- WWCP_OICPv2_0/EMP/EMPServer/EMPServer.cs
+++ WWCP_OICPv2_0/EMP/EMPServer/EMPServer.cs
@@ -650,7 +650,7 @@ namespace org.GraphDefined.WWCP.OICPv2_0
 
                     if (AuthToken.ToString().ToLower() == "049a607a3f3480".ToLower() ||
                         AuthToken.ToString().ToLower() == "dbb32688".ToLower() ||
-                        AuthToken.ToString().ToLower() == "AA3634527A2280".ToLower())
+                        AuthToken.ToString().ToLower() == "BA3634527A2280".ToLower())
                     {
                         Response = AuthStartEVSEResult.Authorized(Authorizator_Id.Parse("lo"), SessionId, EVSP_Id.Parse("DE*GEF"));
                         HubjectCode = "000";
