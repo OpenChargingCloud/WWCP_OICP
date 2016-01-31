d4902bdee47f1376061d3c28fdd0392157d1a063
Achim Friedland <github@ahzf.de>
2016-01-31 01:23:19 +0100

    Minor updates...

--- WWCP_OICPv2_0/EMP/EMPUpstreamService.cs
+++ WWCP_OICPv2_0/EMP/EMPUpstreamService.cs
@@ -35,231 +35,231 @@ using org.GraphDefined.Vanaheimr.Hermod.DNS;
 namespace org.GraphDefined.WWCP.OICPv2_0
 {
 
-    /// <summary>
-    /// OICP v2.0 EMP upstream services.
-    /// </summary>
-    public class EMPUpstreamService
-    {
+    ///// <summary>
+    ///// OICP v2.0 EMP upstream services.
+    ///// </summary>
+    //public class EMPUpstreamService
+    //{
 
-        #region Data
+    //    #region Data
 
-        private readonly EMPClient _EMPClient;
+    //    private readonly EMPClient _EMPClient;
 
-        #endregion
+    //    #endregion
 
-        #region Properties
+    //    #region Properties
 
-        #region AuthorizatorId
+    //    #region AuthorizatorId
 
-        protected readonly Authorizator_Id _AuthorizatorId;
+    //    protected readonly Authorizator_Id _AuthorizatorId;
 
-        public Authorizator_Id AuthorizatorId
-        {
-            get
-            {
-                return _AuthorizatorId;
-            }
-        }
+    //    public Authorizator_Id AuthorizatorId
+    //    {
+    //        get
+    //        {
+    //            return _AuthorizatorId;
+    //        }
+    //    }
 
-        #endregion
+    //    #endregion
 
-        #endregion
+    //    #endregion
 
-        #region Events
+    //    #region Events
 
-        #region OnException
+    //    #region OnException
 
-        /// <summary>
-        /// A delegate called whenever an exception occured.
-        /// </summary>
-        public delegate void OnExceptionDelegate(DateTime Timestamp, Object Sender, Exception Exception);
+    //    /// <summary>
+    //    /// A delegate called whenever an exception occured.
+    //    /// </summary>
+    //    public delegate void OnExceptionDelegate(DateTime Timestamp, Object Sender, Exception Exception);
 
-        /// <summary>
-        /// An event fired whenever an exception occured.
-        /// </summary>
-        public event OnExceptionDelegate OnException;
+    //    /// <summary>
+    //    /// An event fired whenever an exception occured.
+    //    /// </summary>
+    //    public event OnExceptionDelegate OnException;
 
-        #endregion
+    //    #endregion
 
-        #region OnHTTPError
+    //    #region OnHTTPError
 
-        /// <summary>
-        /// A delegate called whenever a HTTP error occured.
-        /// </summary>
-        public delegate void OnHTTPErrorDelegate(DateTime Timestamp, Object Sender, HTTPResponse HttpResponse);
+    //    /// <summary>
+    //    /// A delegate called whenever a HTTP error occured.
+    //    /// </summary>
+    //    public delegate void OnHTTPErrorDelegate(DateTime Timestamp, Object Sender, HTTPResponse HttpResponse);
 
-        /// <summary>
-        /// An event fired whenever a HTTP error occured.
-        /// </summary>
-        public event OnHTTPErrorDelegate OnHTTPError;
+    //    /// <summary>
+    //    /// An event fired whenever a HTTP error occured.
+    //    /// </summary>
+    //    public event OnHTTPErrorDelegate OnHTTPError;
 
-        #endregion
+    //    #endregion
 
-        #endregion
+    //    #endregion
 
-        #region Constructor(s)
+    //    #region Constructor(s)
 
-        /// <summary>
-        /// Create a new OICP v2.0 EMP upstream service.
-        /// </summary>
-        /// <param name="ClientId">A unqiue identification of this client.</param>
-        /// <param name="Hostname">The OICP hostname to connect to.</param>
-        /// <param name="TCPPort">The OICP TCP port to connect to.</param>
-        /// <param name="HTTPVirtualHost">An optional HTTP virtual host name to use.</param>
-        /// <param name="AuthorizatorId">An optional authorizator identification to use.</param>
-        /// <param name="HTTPUserAgent">An optional HTTP user agent to use.</param>
-        /// <param name="QueryTimeout">An optional timeout for upstream queries.</param>
-        /// <param name="DNSClient">An optional DNS client.</param>
-        public EMPUpstreamService(String           ClientId,
-                                  String           Hostname,
-                                  IPPort           TCPPort,
-                                  String           HTTPVirtualHost  = null,
-                                  Authorizator_Id  AuthorizatorId   = null,
-                                  String           HTTPUserAgent    = "GraphDefined OICP v2.0 Gateway EMP Upstream Services",
-                                  TimeSpan?        QueryTimeout     = null,
-                                  DNSClient        DNSClient        = null)
+    //    /// <summary>
+    //    /// Create a new OICP v2.0 EMP upstream service.
+    //    /// </summary>
+    //    /// <param name="ClientId">A unqiue identification of this client.</param>
+    //    /// <param name="Hostname">The OICP hostname to connect to.</param>
+    //    /// <param name="TCPPort">The OICP TCP port to connect to.</param>
+    //    /// <param name="HTTPVirtualHost">An optional HTTP virtual host name to use.</param>
+    //    /// <param name="AuthorizatorId">An optional authorizator identification to use.</param>
+    //    /// <param name="HTTPUserAgent">An optional HTTP user agent to use.</param>
+    //    /// <param name="QueryTimeout">An optional timeout for upstream queries.</param>
+    //    /// <param name="DNSClient">An optional DNS client.</param>
+    //    public EMPUpstreamService(String           ClientId,
+    //                              String           Hostname,
+    //                              IPPort           TCPPort,
+    //                              String           HTTPVirtualHost  = null,
+    //                              Authorizator_Id  AuthorizatorId   = null,
+    //                              String           HTTPUserAgent    = "GraphDefined OICP v2.0 Gateway EMP Upstream Services",
+    //                              TimeSpan?        QueryTimeout     = null,
+    //                              DNSClient        DNSClient        = null)
 
-        {
+    //    {
 
-            this._EMPClient       = new EMPClient(ClientId,
-                                                  Hostname,
-                                                  TCPPort != null ? TCPPort : IPPort.Parse(443),
-                                                  HTTPVirtualHost,
-                                                  HTTPUserAgent,
-                                                  QueryTimeout,
-                                                  DNSClient);
+    //        this._EMPClient       = new EMPClient(ClientId,
+    //                                              Hostname,
+    //                                              TCPPort != null ? TCPPort : IPPort.Parse(443),
+    //                                              HTTPVirtualHost,
+    //                                              HTTPUserAgent,
+    //                                              QueryTimeout,
+    //                                              DNSClient);
 
-            this._AuthorizatorId  = AuthorizatorId;
+    //        this._AuthorizatorId  = AuthorizatorId;
 
-        }
+    //    }
 
-        #endregion
-
-
-        #region PullEVSEData(ProviderId, SearchCenter = null, DistanceKM = 0.0, LastCall = null, QueryTimeout = null)
-
-        /// <summary>
-        /// Create a new task querying EVSE data from the OICP server.
-        /// The request might either have none, 'SearchCenter + DistanceKM' or 'LastCall' parameters.
-        /// Because of limitations at Hubject the SearchCenter and LastCall parameters can not be used at the same time!
-        /// </summary>
-        /// <param name="ProviderId">The unique identification of the EVSP.</param>
-        /// <param name="SearchCenter">An optional geo coordinate of the search center.</param>
-        /// <param name="DistanceKM">An optional search distance relative to the search center.</param>
-        /// <param name="LastCall">An optional timestamp of the last call.</param>
-        /// <param name="QueryTimeout">An optional timeout for this query.</param>
-        public async Task<HTTPResponse<eRoamingEVSEData>>
-
-            PullEVSEData(EVSP_Id           ProviderId,
-                         GeoCoordinate     SearchCenter      = null,
-                         Double            DistanceKM        = 0.0,
-                         DateTime?         LastCall          = null,
-                         TimeSpan?         QueryTimeout      = null)
-
-        {
-
-            return await _EMPClient.PullEVSEData(ProviderId,
-                                                 SearchCenter,
-                                                 DistanceKM,
-                                                 LastCall,
-                                                 QueryTimeout);
-
-        }
-
-        #endregion
+    //    #endregion
 
 
-        #region PullEVSEStatus(ProviderId, SearchCenter = null, DistanceKM = 0, EVSEStatusFilter = null, QueryTimeout = null)
+    //    #region PullEVSEData(ProviderId, SearchCenter = null, DistanceKM = 0.0, LastCall = null, QueryTimeout = null)
 
-        /// <summary>
-        /// Create a new task requesting the current status of all EVSEs (within an optional search radius and status).
-        /// </summary>
-        /// <param name="ProviderId">Your e-mobility provider identification (EMP Id).</param>
-        /// <param name="SearchCenter">An optional geo coordinate of the search center.</param>
-        /// <param name="DistanceKM">An optional search distance relative to the search center.</param>
-        /// <param name="EVSEStatusFilter">An optional EVSE status as filter criteria.</param>
-        /// <param name="QueryTimeout">An optional timeout for this query.</param>
-        public async Task<HTTPResponse<eRoamingEVSEStatus>>
+    //    /// <summary>
+    //    /// Create a new task querying EVSE data from the OICP server.
+    //    /// The request might either have none, 'SearchCenter + DistanceKM' or 'LastCall' parameters.
+    //    /// Because of limitations at Hubject the SearchCenter and LastCall parameters can not be used at the same time!
+    //    /// </summary>
+    //    /// <param name="ProviderId">The unique identification of the EVSP.</param>
+    //    /// <param name="SearchCenter">An optional geo coordinate of the search center.</param>
+    //    /// <param name="DistanceKM">An optional search distance relative to the search center.</param>
+    //    /// <param name="LastCall">An optional timestamp of the last call.</param>
+    //    /// <param name="QueryTimeout">An optional timeout for this query.</param>
+    //    public async Task<HTTPResponse<eRoamingEVSEData>>
 
-            PullEVSEStatus(EVSP_Id              ProviderId,
-                           GeoCoordinate        SearchCenter      = null,
-                           Double               DistanceKM        = 0.0,
-                           EVSEStatusType?  EVSEStatusFilter  = null,
-                           TimeSpan?            QueryTimeout      = null)
+    //        PullEVSEData(EVSP_Id           ProviderId,
+    //                     GeoCoordinate     SearchCenter      = null,
+    //                     Double            DistanceKM        = 0.0,
+    //                     DateTime?         LastCall          = null,
+    //                     TimeSpan?         QueryTimeout      = null)
 
-        {
+    //    {
 
-            return await _EMPClient.PullEVSEStatus(ProviderId,
-                                                   SearchCenter,
-                                                   DistanceKM,
-                                                   EVSEStatusFilter,
-                                                   QueryTimeout);
+    //        return await _EMPClient.PullEVSEData(ProviderId,
+    //                                             SearchCenter,
+    //                                             DistanceKM,
+    //                                             LastCall,
+    //                                             QueryTimeout);
 
-        }
+    //    }
 
-        #endregion
-
-        #region PullEVSEStatusById(ProviderId, EVSEIds, QueryTimeout = null)
-
-        /// <summary>
-        /// Create a new task requesting the current status of up to 100 EVSEs by their EVSE Ids.
-        /// </summary>
-        /// <param name="ProviderId">The unique identification of the EVSP.</param>
-        /// <param name="EVSEIds">Up to 100 EVSE Ids.</param>
-        /// <param name="QueryTimeout">An optional timeout for this query.</param>
-        public async Task<HTTPResponse<eRoamingEVSEStatusById>>
-
-            PullEVSEStatusById(EVSP_Id               ProviderId,
-                               IEnumerable<EVSE_Id>  EVSEIds,
-                               TimeSpan?             QueryTimeout = null)
-
-        {
-
-            return await _EMPClient.PullEVSEStatusById(ProviderId,
-                                                       EVSEIds,
-                                                       QueryTimeout);
-
-        }
-
-        #endregion
+    //    #endregion
 
 
-        #region SearchEVSE(ProviderId, SearchCenter = null, DistanceKM = 0.0, Address = null, Plug = null, ChargingFacility = null, QueryTimeout = null)
+    //    #region PullEVSEStatus(ProviderId, SearchCenter = null, DistanceKM = 0, EVSEStatusFilter = null, QueryTimeout = null)
 
-        /// <summary>
-        /// Create a new Search EVSE request.
-        /// </summary>
-        /// <param name="ProviderId">Your e-mobility provider identification (EMP Id).</param>
-        /// <param name="SearchCenter">An optional geocoordinate of the search center.</param>
-        /// <param name="DistanceKM">An optional search distance relative to the search center.</param>
-        /// <param name="Address">An optional address of the charging stations.</param>
-        /// <param name="Plug">Optional plugs of the charging station.</param>
-        /// <param name="ChargingFacility">Optional charging facilities of the charging station.</param>
-        /// <param name="QueryTimeout">An optional timeout for this query.</param>
-        public async Task<HTTPResponse<eRoamingEvseSearchResult>>
+    //    /// <summary>
+    //    /// Create a new task requesting the current status of all EVSEs (within an optional search radius and status).
+    //    /// </summary>
+    //    /// <param name="ProviderId">Your e-mobility provider identification (EMP Id).</param>
+    //    /// <param name="SearchCenter">An optional geo coordinate of the search center.</param>
+    //    /// <param name="DistanceKM">An optional search distance relative to the search center.</param>
+    //    /// <param name="EVSEStatusFilter">An optional EVSE status as filter criteria.</param>
+    //    /// <param name="QueryTimeout">An optional timeout for this query.</param>
+    //    public async Task<HTTPResponse<eRoamingEVSEStatus>>
 
-            SearchEVSE(EVSP_Id              ProviderId,
-                       GeoCoordinate        SearchCenter      = null,
-                       Double               DistanceKM        = 0.0,
-                       Address              Address           = null,
-                       PlugTypes?           Plug              = null,
-                       ChargingFacilities?  ChargingFacility  = null,
-                       TimeSpan?            QueryTimeout      = null)
+    //        PullEVSEStatus(EVSP_Id              ProviderId,
+    //                       GeoCoordinate        SearchCenter      = null,
+    //                       Double               DistanceKM        = 0.0,
+    //                       EVSEStatusType?  EVSEStatusFilter  = null,
+    //                       TimeSpan?            QueryTimeout      = null)
 
-        {
+    //    {
 
-            return await _EMPClient.SearchEVSE(ProviderId,
-                                               SearchCenter,
-                                               DistanceKM,
-                                               Address,
-                                               Plug,
-                                               ChargingFacility);
+    //        return await _EMPClient.PullEVSEStatus(ProviderId,
+    //                                               SearchCenter,
+    //                                               DistanceKM,
+    //                                               EVSEStatusFilter,
+    //                                               QueryTimeout);
 
-        }
+    //    }
 
-        #endregion
+    //    #endregion
+
+    //    #region PullEVSEStatusById(ProviderId, EVSEIds, QueryTimeout = null)
+
+    //    /// <summary>
+    //    /// Create a new task requesting the current status of up to 100 EVSEs by their EVSE Ids.
+    //    /// </summary>
+    //    /// <param name="ProviderId">The unique identification of the EVSP.</param>
+    //    /// <param name="EVSEIds">Up to 100 EVSE Ids.</param>
+    //    /// <param name="QueryTimeout">An optional timeout for this query.</param>
+    //    public async Task<HTTPResponse<eRoamingEVSEStatusById>>
+
+    //        PullEVSEStatusById(EVSP_Id               ProviderId,
+    //                           IEnumerable<EVSE_Id>  EVSEIds,
+    //                           TimeSpan?             QueryTimeout = null)
+
+    //    {
+
+    //        return await _EMPClient.PullEVSEStatusById(ProviderId,
+    //                                                   EVSEIds,
+    //                                                   QueryTimeout);
+
+    //    }
+
+    //    #endregion
 
 
-    }
+    //    #region SearchEVSE(ProviderId, SearchCenter = null, DistanceKM = 0.0, Address = null, Plug = null, ChargingFacility = null, QueryTimeout = null)
+
+    //    /// <summary>
+    //    /// Create a new Search EVSE request.
+    //    /// </summary>
+    //    /// <param name="ProviderId">Your e-mobility provider identification (EMP Id).</param>
+    //    /// <param name="SearchCenter">An optional geocoordinate of the search center.</param>
+    //    /// <param name="DistanceKM">An optional search distance relative to the search center.</param>
+    //    /// <param name="Address">An optional address of the charging stations.</param>
+    //    /// <param name="Plug">Optional plugs of the charging station.</param>
+    //    /// <param name="ChargingFacility">Optional charging facilities of the charging station.</param>
+    //    /// <param name="QueryTimeout">An optional timeout for this query.</param>
+    //    public async Task<HTTPResponse<eRoamingEvseSearchResult>>
+
+    //        SearchEVSE(EVSP_Id              ProviderId,
+    //                   GeoCoordinate        SearchCenter      = null,
+    //                   Double               DistanceKM        = 0.0,
+    //                   Address              Address           = null,
+    //                   PlugTypes?           Plug              = null,
+    //                   ChargingFacilities?  ChargingFacility  = null,
+    //                   TimeSpan?            QueryTimeout      = null)
+
+    //    {
+
+    //        return await _EMPClient.SearchEVSE(ProviderId,
+    //                                           SearchCenter,
+    //                                           DistanceKM,
+    //                                           Address,
+    //                                           Plug,
+    //                                           ChargingFacility);
+
+    //    }
+
+    //    #endregion
+
+
+    //}
 
 }
