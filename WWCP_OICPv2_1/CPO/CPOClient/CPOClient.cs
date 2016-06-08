1f526370a8101c0f8ab47345b2917f892275f0c3
Achim Friedland <github@ahzf.de>
2016-06-09 01:56:15 +0200

    Eval Reserve/RemoteStart ChargingProduct parameters

--- WWCP_OICPv2_1/CPO/CPOClient/CPOClient.cs
+++ WWCP_OICPv2_1/CPO/CPOClient/CPOClient.cs
@@ -1158,6 +1158,8 @@ namespace org.GraphDefined.WWCP.OICPv2_1
 
                                                     OnSOAPFault: (timestamp, soapclient, httpresponse) => {
 
+                                                        DebugX.Log("e:" + httpresponse.EntirePDU);
+
                                                         SendSOAPError(timestamp, this, httpresponse.Content);
 
                                                         return new HTTPResponse<eRoamingAcknowledgement>(httpresponse,
@@ -1173,6 +1175,8 @@ namespace org.GraphDefined.WWCP.OICPv2_1
                                                     #region OnHTTPError
 
                                                     OnHTTPError: (timestamp, soapclient, httpresponse) => {
+
+                                                        DebugX.Log("e:" + httpresponse.EntirePDU);
 
                                                         SendHTTPError(timestamp, this, httpresponse);
 
@@ -1190,6 +1194,8 @@ namespace org.GraphDefined.WWCP.OICPv2_1
                                                     #region OnException
 
                                                     OnException: (timestamp, sender, exception) => {
+
+                                                        DebugX.Log("e:" + exception.Message);
 
                                                         SendException(timestamp, sender, exception);
 
