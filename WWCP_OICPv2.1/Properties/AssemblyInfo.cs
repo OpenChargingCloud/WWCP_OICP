256be72bd6e5dbe7612ec7da89c27216c3f570f5
Achim Friedland <github@ahzf.de>
2017-05-12 07:55:11 +0200

    version update

--- WWCP_OICPv2.1/Properties/AssemblyInfo.cs
+++ WWCP_OICPv2.1/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WWCP OICP v2.1")]
-[assembly: AssemblyDescription("Communication between the World Wide Charging Protocol (WWCP) and the Open InterCharge Protocol (OICP v2.1).")]
+[assembly: AssemblyDescription("Implementation of the Open InterCharge Protocol (OICP v2.1)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GraphDefined GmbH")]
 [assembly: AssemblyProduct("WWCP OICP v2.1")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
