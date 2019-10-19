55be437df4ceb0e599c74dbd90a99935946d8f9e
Achim Friedland <github@ahzf.de>
2019-10-19 16:57:11 +0200

    Fixes

---
+++ WWCP_OICPv2.2_UnitTests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWCP_OICPv2._2_UnitTests
+{
+    public class Class1
+    {
+    }
+}
