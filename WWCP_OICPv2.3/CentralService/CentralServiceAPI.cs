95d81adccd9eeb4bc2e6bda53d07c4b5978b1d14
Achim Friedland <achim.friedland@graphdefined.com>
2022-09-08 16:00:48 +0200

    Minor refactorings

--- WWCP_OICPv2.3/CentralService/CentralServiceAPI.cs
+++ WWCP_OICPv2.3/CentralService/CentralServiceAPI.cs
@@ -90,17 +90,17 @@ namespace cloud.charging.open.protocols.OICPv2_3.CentralService
         /// <summary>
         /// An event called whenever a HTTP request came in.
         /// </summary>
-        public HTTPRequestLogEvent   RequestLog    = new ();
+        public HTTPRequestLogEvent   RequestLog    = new();
 
         /// <summary>
         /// An event called whenever a HTTP request could successfully be processed.
         /// </summary>
-        public HTTPResponseLogEvent  ResponseLog   = new ();
+        public HTTPResponseLogEvent  ResponseLog   = new();
 
         /// <summary>
         /// An event called whenever a HTTP request resulted in an error.
         /// </summary>
-        public HTTPErrorLogEvent     ErrorLog      = new ();
+        public HTTPErrorLogEvent     ErrorLog      = new();
 
         #endregion
 
